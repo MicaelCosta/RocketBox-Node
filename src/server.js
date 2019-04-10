94f7a722c086e08f3b8ca5938cd84567afc442a3
MicaelCosta <micaelcosta97@hotmail.com>
2019-04-09 22:47:25 -0300

    Adiciona variaveis ambiente

--- src/server.js
+++ src/server.js
@@ -42,4 +42,4 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'temp')));
 //Importa o arquivo de rotas
 app.use(require('./routes'));
 
-server.listen(3333);
+server.listen(process.env.PORT || 3333);
